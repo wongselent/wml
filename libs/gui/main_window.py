1fb262695efd3f9a6cd8c3bac83189b6b2f10ef0
wongselent <david.agung.satrio@gmail.com>
2020-06-23 08:44:14 +0800

    update all

--- libs/gui/main_window.py
+++ libs/gui/main_window.py
@@ -4,7 +4,7 @@ from typing import Dict
 from PyQt5 import QtCore, QtWidgets
 
 from libs import config, thread
-from libs.gui.components import tabbar_widget, video_widget
+from libs.gui.components import tabbar_widget, video_widget, social_widget
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -17,20 +17,17 @@ class MainWindow(QtWidgets.QMainWindow):
         self.main_tabwidget.setTabText(0, "WML")
         # self.main_tabwidget.setTabsClosable(True)
 
-        # self.__main_tab = tabbar_widget.TabBar(
-        #     parent=self.main_tabwidget,
-        #     tab_title="WML",
-        #     tab_widget=None
-        # )
+        self.__main_tab = tabbar_widget.TabBar(
+            parent=self.main_tabwidget,
+            tab_title="WML",
+            tab_widget=None
+        )
 
-        # test_thread.run_func(self.__test_worker)
-
-        # TODO: tab lagi dibuat
-        # self.__social_looter_tab = tabbar_widget.TabBar(
-        #     parent=self.main_tabwidget,
-        #     tab_title="Social Looter",
-        #     tab_widget=social_widget.SocialLooterWidget(self)
-        # )
+        self.__social_looter_tab = tabbar_widget.TabBar(
+            parent=self.main_tabwidget,
+            tab_title="Social Looter",
+            tab_widget=social_widget.SocialLooterWidget(self)
+        )
 
         self.__create_video_tab = tabbar_widget.TabBar(
             parent=self.main_tabwidget,
@@ -42,26 +39,12 @@ class MainWindow(QtWidgets.QMainWindow):
         pass
 
 
-def run(argv: Dict[str, str] = None):
+def run(argv: Dict[str, str] = None) -> None:
     if len(argv) > 1:
         sys.exit(argv)
 
     app = QtWidgets.QApplication([])
     win = MainWindow()
     win.show()
+    win.raise_()
     sys.exit(app.exec_())
-
-    # def __test(self):
-    #     from libs import video_editor as veditor
-    #
-    #     rd = veditor.RenderVideo("/home/wongselent/Videos/memes_1", bg_blur=True)
-    #     rd.setSize(width=1280, height=720, reduce=3)
-    #
-    #     label = QtWidgets.QLabel(self)
-    #     image = rd.getFrameImage(label)
-    #
-    #     btn = QtWidgets.QPushButton("next", self)
-    #     btn.clicked.connect(lambda: next(image))
-    #
-    #     self.addWidget(label)
-    #     self.addWidget(btn)
